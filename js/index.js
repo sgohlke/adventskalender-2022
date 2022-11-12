7c7ee5749812763353286a19e97a0ba074b3744a
stefangohlke <gohlke.stefan@yahoo.de>
2022-11-12 21:18:03 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+function init(){
+    console.log('Init done')
+
+    
+
+
+    const calendar = document.getElementById('calendar')
+    if (calendar) {
+
+        // Create number array for days 1 to 24
+        let days = [];
+        for (let day = 1; day <= 24; day++) {
+            days.push(day)
+        }
+
+        console.log('days original', days)
+
+        // Shuffle days
+        days = days.sort(() => Math.random() - 0.5);
+
+        console.log('days shuffled', days)
+        
+        // Add day elements to calendar
+        for (const day of days) {
+            const door = document.createElement("section");
+            door.classList.add('doorItem')
+            door.innerHTML = `<p>${day}</p>`;
+            calendar.appendChild(door);
+        }
+    }
+}
