04ef829e384d5e6530f15062bb8e9ac30251895e
GreenBoyGreen <111475280+sucantienunderscore@users.noreply.github.com>
2022-11-12 21:30:20 +0100

    Fix Background Image and remove init function

--- js/index.js
+++ js/index.js
@@ -1,32 +1,24 @@
+const calendar = document.getElementById('calendar')
+if (calendar) {
 
-function init(){
-    console.log('Init done')
+    // Create number array for days 1 to 24
+    let days = [];
+    for (let day = 1; day <= 24; day++) {
+        days.push(day)
+    }
 
+    console.log('days original', days)
+
+    // Shuffle days
+    days = days.sort(() => Math.random() - 0.5);
+
+    console.log('days shuffled', days)
     
-
-
-    const calendar = document.getElementById('calendar')
-    if (calendar) {
-
-        // Create number array for days 1 to 24
-        let days = [];
-        for (let day = 1; day <= 24; day++) {
-            days.push(day)
-        }
-
-        console.log('days original', days)
-
-        // Shuffle days
-        days = days.sort(() => Math.random() - 0.5);
-
-        console.log('days shuffled', days)
-        
-        // Add day elements to calendar
-        for (const day of days) {
-            const door = document.createElement("section");
-            door.classList.add('doorItem')
-            door.innerHTML = `<p>${day}</p>`;
-            calendar.appendChild(door);
-        }
+    // Add day elements to calendar
+    for (const day of days) {
+        const door = document.createElement("section");
+        door.classList.add('doorItem')
+        door.innerHTML = `<p>${day}</p>`;
+        calendar.appendChild(door);
     }
 }
