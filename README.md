8a054d0b449b22d13b8e1e386b29e76d4b8263a0
stefangohlke <gohlke.stefan@yahoo.de>
2022-11-12 22:05:55 +0100

    Add first showContent logic

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # adventskalender-2022
 An advent calendar app
+
+# How to run
+You can run it on the [Github pages](https://sgohlke.github.io/adventskalender-2022/) page of this project.
--- js/index.js
+++ js/index.js
@@ -1,24 +1,33 @@
 const calendar = document.getElementById('calendar')
 if (calendar) {
-
     // Create number array for days 1 to 24
     let days = [];
     for (let day = 1; day <= 24; day++) {
         days.push(day)
     }
 
-    console.log('days original', days)
-
     // Shuffle days
     days = days.sort(() => Math.random() - 0.5);
-
-    console.log('days shuffled', days)
     
     // Add day elements to calendar
     for (const day of days) {
         const door = document.createElement("section");
         door.classList.add('doorItem')
-        door.innerHTML = `<p>${day}</p>`;
+        door.innerHTML = `<p onclick='showContent(${day})'>${day}</p>`;
         calendar.appendChild(door);
+    }
+}
+
+function showContent(day) {
+    const today = new Date();
+    // Note: getMonth starts at 0 so december is value 11
+    if (today.getFullYear() === 2022 && today.getMonth() === 11) {
+        if (today.getDate() >= day) {
+            alert('Not yet implemented')
+        } else {
+            alert(`You cannot open the door ${day} for yet!`)
+        }
+    } else {
+        alert('You can only open the advent calendar in the year 2022 and the month December')
     }
 }
